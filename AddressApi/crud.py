011518b75b78e762cf6bece6233301868d0961d1
Yashkumar <yverulkar02@gmail.com>
2022-09-21 10:08:16 +0530

    Fetching Address Based on distance Added

--- AddressApi/crud.py
+++ AddressApi/crud.py
@@ -1,12 +1,9 @@
 from sqlalchemy.orm import Session
 from models import AddressModel
 
-#create_friend
+
 def create_Address(db:Session, name, lat, lng):
-    """
-    function to create a friend model object
-    """
-    # create friend instance 
+    # create Address instance 
     new_address = AddressModel(name=name, lat=lat, lng=lng)
     #place object in the database session
     db.add(new_address)
@@ -15,39 +12,26 @@ def create_Address(db:Session, name, lat, lng):
     #refresh the attributes of the given instance
     db.refresh(new_address)
     return new_address
-#get_friend
+
 def get_Address(db:Session, id:int):
-    """
-    get the first record with a given id, if no such record exists, will return null
-    """
     data = db.query(AddressModel).filter(AddressModel.id==id).first()
     return data
-#ist_friends
+
 def list_Address(db:Session):
-    """
-    Return a list of all existing Friend records
-    """
     all_address = db.query(AddressModel).all()
     return all_address
 
-#update_friend
+
 def update_Address(db:Session, id:int,name: str, lat: float, lng:float):
-    """
-    Update a Friend object's attributes
-    """
     update_data = get_Address(db=db, id=id)
     update_data.name = name
     update_data.lat = lat
     update_data.lng = lng
-
     db.commit()
     db.refresh(update_data ) #refresh the attribute of the given instance
     return update_data 
-#elete_friend
+
 def delete_Address(db:Session, id:int):
-    """
-    Delete a Friend object
-    """
     delete_data  = get_Address(db=db, id=id)
     db.delete(delete_data )
     db.commit() #save changes to db
--- AddressApi/main.py
+++ AddressApi/main.py
@@ -3,6 +3,7 @@ import models
 import crud
 from crud import Session
 from db import engine, SessionLocal
+from math import radians, cos, sin, asin, sqrt
 
 #create the database tables on app startup or reload
 models.Base.metadata.create_all(bind=engine)
@@ -18,22 +19,45 @@ def get_db():
     finally:
         db.close()
 
-#define endpoint
-# @app.get("/")
-# def getallAddress():
-#     return {"Ahoy": "Captain"}
-
 #get all Addresses
-@app.get("/")
-def list_Address(db:Session = Depends(get_db)):
-    Address_list = crud.list_Address(db=db)
-    return Address_list
+# @app.get("/")
+# def list_Address(db:Session = Depends(get_db)):
+#     Address_list = crud.list_Address(db=db)
+#     return Address_list
 
 #get by id
 @app.get("/{id}") #id is a path parameter
 def get_Address(id:int, db:Session = Depends(get_db)):
     data = crud.get_Address(db=db, id=id)
     return data
+
+@app.get("/")
+def get_custom_address(distance:int,lat:float, lng:float, db:Session = Depends(get_db)):
+    lat = radians(lat)
+    lng = radians(lng)
+    #load all addresses
+    data=crud.list_Address(db=db)
+    result={}
+        #logic for getting custom data
+    for d in data:
+            lat2 = radians(d.lat)
+            lng2 = radians(d.lng)
+            # Haversine formula
+            dlng = lng2 - lng
+            dlat = lat2 - lat
+            a = sin(dlat / 2)**2 + cos(lat) * cos(lat2) * sin(dlng / 2)**2
+            c = 2 * asin(sqrt(a))
+            # Radius of earth in kilometers. Use 3956 for miles
+            r = 6371
+            # calculate the result
+            dist =c * r
+            if distance>=dist:
+            # return d
+                result[d.id]=(d.name,d.lat,d.lng)
+    if result:
+        return result
+    else:
+        return {"error": "Address does not exist"}
 
 #create new address
 @app.post("/")
@@ -44,10 +68,10 @@ def create_Address(name:str, lat:float, lng:float, db:Session = Depends(get_db))
 
 #Update Address
 @app.put("/{id}") #id is a path parameter
-def update_friend(id:int, name:str, lat:float, lng:float, db:Session=Depends(get_db)):
-    #get friend object from database
+def update_Address(id:int, name:str, lat:float, lng:float, db:Session=Depends(get_db)):
+    #get object from database
     old_data = crud.get_Address(db=db, id=id)
-    #check if friend object exists
+    #check if  object exists
     if old_data:
         updated_data = crud.update_Address(db=db, id=id, name=name, lat=lat, lng=lng)
         return updated_data
@@ -56,9 +80,9 @@ def update_friend(id:int, name:str, lat:float, lng:float, db:Session=Depends(get
 
 @app.delete("/{id}") #id is a path parameter
 def delete_Address(id:int, db:Session=Depends(get_db)):
-    #get friend object from database
+    #get  object from database
     data = crud.get_Address(db=db, id=id)
-    #check if friend object exists
+    #check if object exists
     if data:
         return crud.delete_Address(db=db, id=id)
     else:
